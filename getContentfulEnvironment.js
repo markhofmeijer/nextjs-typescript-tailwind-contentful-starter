92762277b66d58b49b9ae35167cba89894cc5882
Mark Hofmeijer <m.hofmeijer@click-designs.nl>
2021-01-16 11:58:44 +0100

    Optimizations for Contentful type generator

--- getContentfulEnvironment.js
+++ getContentfulEnvironment.js
@@ -4,10 +4,10 @@ const contentful = require("contentful-management")
 
 module.exports = function () {
   const client = contentful.createClient({
-    accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_PERSONAL_ACCESS_TOKEN,
+    accessToken: process.env.CONTENTFUL_MANAGEMENT_API_ACCESS_TOKEN,
   })
 
   return client
-    .getSpace(process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID)
-    .then(space => space.getEnvironment(process.env.NEXT_PUBLIC_CONTENTFUL_ENVIRONMENT))
+    .getSpace(process.env.CONTENTFUL_SPACE_ID)
+    .then(space => space.getEnvironment(process.env.CONTENTFUL_ENVIRONMENT))
 }
