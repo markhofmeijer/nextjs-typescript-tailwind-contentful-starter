014d240908070e0c29836e6097a515420027efb8
Mark Hofmeijer <m.hofmeijer@click-designs.nl>
2021-01-19 01:48:14 +0100

    Added pwa support via next-pwa

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
-module.exports = {
+const withPWA = require("next-pwa")
+
+module.exports = withPWA({
   images: {
     domains: ["images.ctfassets.net"],
   },
-}
+  pwa: {
+    dest: "public",
+  },
+})
--- src/components/elements/Head/Head.tsx
+++ src/components/elements/Head/Head.tsx
@@ -30,6 +30,7 @@ const Head: React.FC<IAppDataProps> = ({ data }) => {
       <meta name="description" content={description} />
       <meta name="image" content={image.url} />
       <meta name="keywords" content={keywords.join(",")} />
+
       <meta property="og:title" content={title} />
       <meta property="og:url" content={url} />
       <meta property="og:image" content={image.url} />
@@ -42,6 +43,18 @@ const Head: React.FC<IAppDataProps> = ({ data }) => {
       <meta property="twitter:card" content="summary_large_image" />
       {/* <meta property="twitter:site" content={} />
       <meta property="twitter:creator" content={} /> */}
+
+      <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+      <link rel="manifest" href="/manifest.json" />
+      <link rel="apple-touch-icon" sizes="72x72" href="/icons/icon-72x72.png" />
+      <link rel="apple-touch-icon" sizes="96x96" href="/icons/icon-96x96.png" />
+      <link rel="apple-touch-icon" sizes="128x128" href="/icons/icon-128x128.png" />
+      <link rel="apple-touch-icon" sizes="144x144" href="/icons/icon-144x144.png" />
+      <link rel="apple-touch-icon" sizes="152x152" href="/icons/icon-152x152.png" />
+      <link rel="apple-touch-icon" sizes="192x192" href="/icons/icon-192x192.png" />
+      <link rel="apple-touch-icon" sizes="384x384" href="/icons/icon-384x384.png" />
+      <link rel="apple-touch-icon" sizes="512x512" href="/icons/icon-512x512.png" />
+      <meta name="theme-color" content="#afab96" />
     </NextHead>
   )
 }
