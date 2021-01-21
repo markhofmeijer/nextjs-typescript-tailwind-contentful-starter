edb239d98c72ff92951f05efc300f126b855c99f
Mark Hofmeijer <m.hofmeijer@click-designs.nl>
2021-01-22 00:45:52 +0100

    Removed unused code

--- src/types/seo.d.ts
+++ src/types/seo.d.ts
@@ -8,18 +8,3 @@ export interface ISEO {
   image?: IMediaImage | null
   keywords?: string[] | null
 }
-
-// export interface ISEOProps {
-//   title?: string | undefined
-//   description?: string | undefined
-//   url?: string | undefined
-//   author?: string | undefined
-//   image?: string | undefined
-//   keywords?: string[] | undefined
-//   meta?: IMetaItem[] | undefined
-// }
-
-// interface IMetaItem {
-//   name: string | undefined
-//   content: string | undefined
-// }
