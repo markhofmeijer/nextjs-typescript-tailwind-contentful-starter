11c5752c8e292e2adeda26e4d4960e884fc79f18
Mark Hofmeijer <m.hofmeijer@click-designs.nl>
2021-02-08 20:27:16 +0100

    Removed next-on-netlify

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@ const isProd = process.env.NODE_ENV === "production"
 const withPWA = require("next-pwa")
 
 module.exports = withPWA({
-  target: "serverless",
   images: {
     domains: ["images.ctfassets.net"],
   },
