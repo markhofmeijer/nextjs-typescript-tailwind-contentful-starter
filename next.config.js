79297373335463ad3c180d10387998e2f975bb85
Mark Hofmeijer <m.hofmeijer@click-designs.nl>
2021-02-08 18:42:12 +0100

    Add next-on-netlify

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const isProd = process.env.NODE_ENV === "production"
 const withPWA = require("next-pwa")
 
 module.exports = withPWA({
+  target: "serverless",
   images: {
     domains: ["images.ctfassets.net"],
   },
