943c61b569ea573afd159e088e095b80d27b0821
Mark Hofmeijer <m.hofmeijer@click-designs.nl>
2021-11-26 22:11:36 +0100

    Use preact on production builds

--- next.config.js
+++ next.config.js
@@ -4,4 +4,15 @@ module.exports = {
   images: {
     domains: ["images.ctfassets.net"],
   },
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: "preact/compat",
+        "react-dom/test-utils": "preact/test-utils",
+        "react-dom": "preact/compat",
+      })
+    }
+
+    return config
+  },
 }
